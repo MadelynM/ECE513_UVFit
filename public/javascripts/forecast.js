2f3ac995c39336c99b9088f9a15f297bcc3f5234
Safwan <safwanelmadani@email.arizona.edu>
2018-11-23 20:21:29 -0700

    Update weather

--- public/javascripts/forecast.js
+++ public/javascripts/forecast.js
@@ -3,81 +3,69 @@ function sendReqForWeatherInfo() {
 
   console.log();
     $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/forecast?q=Tucson,us&APPID=c8672c5b456801221785b70e00fa460a&units=Imperial',
+        url: 'https://api.apixu.com/v1/forecast.json?key=d4f6c721c04d47c6bd1221550182311&q=Tucson&days=10',
         type: 'GET',
         data: { }, //, email: $("#email").text()
         responseType: 'json',
         success: function (data, textStatus, jqXHR) {
-          var city=data.city.name;
-          var fiveDaysForecast1=data.list[5];
-        //  var fiveDaysForecast2=data.list[5];
-          //var fiveDaysForecast3=data.list[5];
-           var utcSeconds = fiveDaysForecast1.dt;
-          console.log(data);
-          var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-             d.setUTCSeconds(utcSeconds);
-                 // days[d.getDay()];
-// the first card:
-           var icon="";
-           icon="<img class=\"responsive-img\" src= http://openweathermap.org/img/w/"
-           icon=icon+ fiveDaysForecast1.weather[0].icon;
-           icon=icon+ ".png >";
+          var forecast=data.forecast.forecastday[1];
+         console.log(data);
+         var date=forecast.date;
+         var dateChanged = date.split("-").join("/");
+          var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
 
-        $("#img1").html(icon);
-        $("#des1").html(fiveDaysForecast1.weather[0].description);
-        $("#day1").html(days[d.getDay()]);
-        $("#date1").html(d);
-        $("#temp1").html("Temp: " +fiveDaysForecast1.main.temp+ "&#x2109;");
-        $("#Humidity1").html( "Humidity: "+fiveDaysForecast1.main.humidity+ "%");
-        $("#wind1").html("wind: "+fiveDaysForecast1.wind.speed+"m/h");
-//////////////////
+// // the first card:
+            var icon="";
+           icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
+           icon=icon+ ">";
+         $("#img1").html(icon);
+         $("#des1").html(forecast.day.condition.text);
+         $("#day1").html(days[d.getDay()]);
+         $("#date1").html(forecast.date);
+         $("#temp1").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
+         $("#Humidity1").html( "Humidity: "+forecast.day.avghumidity+ "%");
+         $("#wind1").html("wind: "+forecast.day.maxwind_mph+" m/h");
+          $("#uv1").html("UV index: "+forecast.day.uv);
+// //////////////////
+         var forecast=data.forecast.forecastday[2];
+         console.log(forecast);
+         var date=forecast.date;
+         var dateChanged = date.split("-").join("/");
+          var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
 
-        var fiveDaysForecast1=data.list[13];
-//  var fiveDaysForecast2=data.list[5];
-//var fiveDaysForecast3=data.list[5];
-        var utcSeconds = fiveDaysForecast1.dt;
+// // the first card:
+            var icon="";
+           icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
+           icon=icon+ ">";
+         $("#img2").html(icon);
+         $("#des2").html(forecast.day.condition.text);
+         $("#day2").html(days[d.getDay()]);
+         $("#date2").html(forecast.date);
+         $("#temp2").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
+         $("#Humidity2").html( "Humidity: "+forecast.day.avghumidity+ "%");
+         $("#wind2").html("wind: "+forecast.day.maxwind_mph+" m/h");
+          $("#uv2").html("UV index: "+forecast.day.uv);
+//         ///////////////////////////////////
+//
+var forecast=data.forecast.forecastday[3];
+console.log(forecast);
+var date=forecast.date;
+var dateChanged = date.split("-").join("/");
+ var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
 
-        var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        d.setUTCSeconds(utcSeconds);
-       // days[d.getDay()];
-
-// the first card:
-        var icon="";
-        icon="<img class=\"responsive-img\" src= http://openweathermap.org/img/w/"
-        icon=icon+ fiveDaysForecast1.weather[0].icon;
-        icon=icon+ ".png >";
-
-        $("#img2").html(icon);
-        $("#des2").html(fiveDaysForecast1.weather[0].description);
-        $("#day2").html(days[d.getDay()]);
-        $("#date2").html(d);
-        $("#temp2").html("Temp: " +fiveDaysForecast1.main.temp+ "&#x2109;");
-        $("#Humidity2").html( "Humidity: "+fiveDaysForecast1.main.humidity+ "%");
-        $("#wind2").html("wind: "+fiveDaysForecast1.wind.speed+"m/h");
-        ///////////////////////////////////
-
-        var fiveDaysForecast1=data.list[21];
-        //  var fiveDaysForecast2=data.list[5];
-        //var fiveDaysForecast3=data.list[5];
-         var utcSeconds = fiveDaysForecast1.dt;
-
-        var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-           d.setUTCSeconds(utcSeconds);
-               // days[d.getDay()];
-
-        // the first card:
-         var icon="";
-         icon="<img class=\"responsive-img\" src= http://openweathermap.org/img/w/"
-         icon=icon+ fiveDaysForecast1.weather[0].icon;
-         icon=icon+ ".png >";
-
-        $("#img3").html(icon);
-        $("#des3").html(fiveDaysForecast1.weather[0].description);
-        $("#day3").html(days[d.getDay()]);
-          $("#date3").html(d);
-            $("#temp3").html("Temp: " +fiveDaysForecast1.main.temp+ "&#x2109;");
-              $("#Humidity3").html( "Humidity: "+fiveDaysForecast1.main.humidity+ "%");
-                $("#wind3").html("wind: "+fiveDaysForecast1.wind.speed+"m/h");
+// // the first card:
+   var icon="";
+  icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
+  icon=icon+ ">";
+$("#img3").html(icon);
+$("#des3").html(forecast.day.condition.text);
+$("#day3").html(days[d.getDay()]);
+$("#date3").html(forecast.date);
+$("#temp3").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
+$("#Humidity3").html( "Humidity: "+forecast.day.avghumidity+ "%");
+$("#wind3").html("wind: "+forecast.day.maxwind_mph+" m/h");
+ $("#uv3").html("UV index: "+forecast.day.uv);
+//         ///////////////////////////////////
 
         },
         error: function(jqXHR, textStatus, errorThrown) {
