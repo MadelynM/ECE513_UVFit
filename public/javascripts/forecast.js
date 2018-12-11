3d58a24ae8e412341a4c62463a360a85f24fdb91
Safwan <safwanelmadani@email.arizona.edu>
2018-12-11 02:30:52 -0700

    update forecast

--- public/javascripts/forecast.js
+++ public/javascripts/forecast.js
@@ -3,68 +3,74 @@ function sendReqForWeatherInfo() {
 
   console.log();
     $.ajax({
-        url: 'https://api.apixu.com/v1/forecast.json?key=d4f6c721c04d47c6bd1221550182311&q=Tucson&days=10',
+        url: 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/3d381662b0a6453183f4e99e6dd41ef2/32.2226,-110.9747',
         type: 'GET',
         data: { }, //, email: $("#email").text()
         responseType: 'json',
         success: function (data, textStatus, jqXHR) {
-          var forecast=data.forecast.forecastday[1];
-         console.log(data);
-         var date=forecast.date;
-         var dateChanged = date.split("-").join("/");
-          var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
-
-// // the first card:
-            var icon="";
-           icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
-           icon=icon+ ">";
-         $("#img1").html(icon);
-         $("#des1").html(forecast.day.condition.text);
-         $("#day1").html(days[d.getDay()]);
-         $("#date1").html(forecast.date);
-         $("#temp1").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
-         $("#Humidity1").html( "Humidity: "+forecast.day.avghumidity+ "%");
-         $("#wind1").html("wind: "+forecast.day.maxwind_mph+" m/h");
-          $("#uv1").html("UV index: "+forecast.day.uv);
-// //////////////////
-         var forecast=data.forecast.forecastday[2];
+         var forecast=data.daily.data[1];
          console.log(forecast);
-         var date=forecast.date;
-         var dateChanged = date.split("-").join("/");
-          var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
+         var date=forecast.time;
+      //   var dateChanged = date.split("-").join("/");
+  //    var utcSeconds = 1234567890;
+        var d = new Date(0); // The 0 there is the key, which sets the date to
 
+         d.setUTCSeconds(date);
 // // the first card:
-            var icon="";
-           icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
-           icon=icon+ ">";
-         $("#img2").html(icon);
-         $("#des2").html(forecast.day.condition.text);
-         $("#day2").html(days[d.getDay()]);
-         $("#date2").html(forecast.date);
-         $("#temp2").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
-         $("#Humidity2").html( "Humidity: "+forecast.day.avghumidity+ "%");
-         $("#wind2").html("wind: "+forecast.day.maxwind_mph+" m/h");
-          $("#uv2").html("UV index: "+forecast.day.uv);
-//         ///////////////////////////////////
-//
-var forecast=data.forecast.forecastday[3];
-console.log(forecast);
-var date=forecast.date;
-var dateChanged = date.split("-").join("/");
- var d = new Date(dateChanged); // The 0 there is the key, which sets the date to the epoch
+//             var icon="";
+//            icon="<img class=\"responsiveC-img\" src=" +forecast.day.condition.icon;
+//            icon=icon+ ">";
+//          $("#img1").html(icon);
+          $("#des1").html("Summary: "+forecast.summary);
+          $("#day1").html(days[d.getDay()]);
+          $("#date1").html(d);
+          $("#temp1").html("Temp: " +forecast.apparentTemperatureHigh+ "&#x2109;");
+          $("#Humidity1").html( "Humidity: "+forecast.humidity*100+ "%");
+          $("#wind1").html("Wind speed: "+forecast.windSpeed+" m/h");
+          $("#uv1").html("UV index: "+forecast.uvIndex);
+// // //////////////////
+        var forecast=data.daily.data[2];
+        console.log(forecast);
+        var date=forecast.time;
+//   var dateChanged = date.split("-").join("/");
+//    var utcSeconds = 1234567890;
+          var d = new Date(0); // The 0 there is the key, which sets the date to
 
+          d.setUTCSeconds(date);
 // // the first card:
-   var icon="";
-  icon="<img class=\"responsive-img\" src=" +forecast.day.condition.icon;
-  icon=icon+ ">";
-$("#img3").html(icon);
-$("#des3").html(forecast.day.condition.text);
-$("#day3").html(days[d.getDay()]);
-$("#date3").html(forecast.date);
-$("#temp3").html("Temp: " +forecast.day.avgtemp_f+ "&#x2109;");
-$("#Humidity3").html( "Humidity: "+forecast.day.avghumidity+ "%");
-$("#wind3").html("wind: "+forecast.day.maxwind_mph+" m/h");
- $("#uv3").html("UV index: "+forecast.day.uv);
+//             var icon="";
+//            icon="<img class=\"responsiveC-img\" src=" +forecast.day.condition.icon;
+//            icon=icon+ ">";
+//          $("#img1").html(icon);
+           $("#des2").html("Summary: "+forecast.summary);
+           $("#day2").html(days[d.getDay()]);
+           $("#date2").html(d);
+           $("#temp2").html("Temp: " +forecast.apparentTemperatureHigh+ "&#x2109;");
+           $("#Humidity2").html( "Humidity: "+forecast.humidity*100+ "%");
+           $("#wind2").html("Wind speed: "+forecast.windSpeed+" m/h");
+           $("#uv2").html("UV index: "+forecast.uvIndex);
+
+// //
+            var forecast=data.daily.data[3];
+          console.log(forecast);
+            var date=forecast.time;
+//   var dateChanged = date.split("-").join("/");
+//    var utcSeconds = 1234567890;
+            var d = new Date(0); // The 0 there is the key, which sets the date to
+
+            d.setUTCSeconds(date);
+// // the first card:
+//             var icon="";
+//            icon="<img class=\"responsiveC-img\" src=" +forecast.day.condition.icon;
+//            icon=icon+ ">";
+//          $("#img1").html(icon);
+            $("#des3").html("Summary: "+forecast.summary);
+            $("#day3").html(days[d.getDay()]);
+            $("#date3").html(d);
+            $("#temp3").html("Temp: " +forecast.apparentTemperatureHigh+ "&#x2109;");
+            $("#Humidity3").html( "Humidity: "+forecast.humidity*100+ "%");
+            $("#wind3").html("Wind speed: "+forecast.windSpeed+" m/h");
+            $("#uv3").html("UV index: "+forecast.uvIndex);
 //         ///////////////////////////////////
 
         },
